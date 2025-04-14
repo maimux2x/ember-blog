9d11d1ace3d1e6219f3a2d182c1c733b08a03991
maimux2x <m0smile10often@gmail.com>
2025-04-14 17:17:28 +0900

    Markdown

---
+++ web/app/components/post.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { marked } from 'marked';
+import { htmlSafe } from '@ember/template';
+import sanitizeHtml from 'sanitize-html';
+
+export default class PostComponent extends Component {
+  @action
+  renderMarkdown(body) {
+    return htmlSafe(sanitizeHtml(marked.parse(body)));
+  }
+}
